4b1303edd221dfb71b10f473f01a74201c626543
Shing Chan <chan.shing@protonmail.com>
2024-04-04 00:30:43 +0000

    fix: Assign virtualenv creation and add comment source documentation in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh -l
-
+# Implements the build steps from the following code:
+# https://github.com/clayrisser/linux-factory/blob/main/.gitlab-ci.yml
 FACTORY_DIR="$GITHUB_WORKSPACE/$1"
 cd $FACTORY_DIR
+sudo virtualenv env
 sudo make build
